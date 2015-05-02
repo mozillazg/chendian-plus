efcf230a8d6c783e4da5eae8bb3467a30e46874c
mozillazg <opensource.mozillazg@gmail.com>
2015-05-02 11:51:52 +0800

    api 权限 closed #10

--- chendian/api/book/views.py
+++ chendian/api/book/views.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-from rest_framework import serializers
 from rest_framework.generics import (
     ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 )
@@ -10,23 +9,7 @@ from rest_framework.generics import (
 from api.qq.serializers import CheckinSerializer
 from book.models import Book
 from qq.models import CheckinRecord
-
-
-class BookSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Book
-        fields = ('id', 'name', 'douban_url', 'description',
-                  'created_at', 'updated_at', 'cover', 'isbn',
-                  'last_read_at', 'read_count')
-        read_only_fields = (
-            'id', 'created_at', 'updated_at', 'last_read_at', 'read_count'
-        )
-        extra_kwargs = {
-            'description': {'required': False},
-            'douban_url': {'required': False},
-            'cover': {'required': False},
-            'isbn': {'required': False},
-        }
+from .serializers import BookSerializer
 
 
 class BookList(ListCreateAPIView):
--- chendian/chendian/settings.py
+++ chendian/chendian/settings.py
@@ -138,7 +138,7 @@ REST_FRAMEWORK = {
         'rest_framework.filters.DjangoFilterBackend',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'api.permissions.IsAdminOrReadonly',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
