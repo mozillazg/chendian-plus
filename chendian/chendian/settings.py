52febebd1237b5e26b2e8cb650f8b16aec7d68ef
mozillazg <opensource.mozillazg@gmail.com>
2015-05-04 21:36:24 +0800

    change static url

--- chendian/chendian/settings.py
+++ chendian/chendian/settings.py
@@ -40,7 +40,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.media",
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
-    "django.contrib.messages.context_processors.messages"
+    "django.contrib.messages.context_processors.messages",
+    "django.template.context_processors.debug",
 )
 
 ALLOWED_HOSTS = []
