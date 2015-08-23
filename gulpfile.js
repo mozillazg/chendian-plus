2c800f6019fa750fd18eacb498150f3784716115
mozillazg <opensource.mozillazg@gmail.com>
2015-08-23 18:52:15 +0800

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,14 @@ var rev = require('gulp-rev');
 
 gulp.task('default', ['watch']);
 
+gulp.task('css', function() {
+  var dest = './chendian/static/css/frontend/';
+  del(dest + '*.css');
+  return gulp.src('./chendian/static/css/src/*.css')
+    .pipe(rev())
+    .pipe(gulp.dest(dest))
+});
+
 gulp.task('coffee', function() {
   var dest = './chendian/static/js/frontend/normal/';
   del(dest + '*.js');
@@ -28,19 +36,53 @@ gulp.task('react', function() {
     .pipe(gulp.dest(dest));
 });
 
-gulp.task('replace', ['coffee', 'react'], function() {
-  return glob('./chendian/static/js/frontend/**/*.js', function(er, files) {
+gulp.task('replace', ['css', 'coffee', 'react'], function() {
+  var regexpFrontendJS = function (jsName){
+    return new RegExp('(css\/frontend\/)' + escapeRegExp(jsName) + '[^\/"\']*\.js', 'ig');
+  }
+  replaceStaticFiles(
+    './chendian/static/js/frontend/**/*.js',
+    regexpFrontendJS,
+    './chendian/templates/frontend/**/*.html'
+  )
+
+  var regexpFrontendCSS = function (cssName){
+    return new RegExp('(css\/frontend\/)' + escapeRegExp(cssName) + '[^\/"\']*\.css', 'ig');
+  }
+  replaceStaticFiles(
+    './chendian/static/css/frontend/**/*.css',
+    regexpFrontendCSS,
+    './chendian/templates/frontend/**/*.html'
+  )
+
+});
+
+gulp.task('watch', function() {
+  return gulp.watch(
+    ['./chendian/static/js/**/*.jsx', './chendian/static/js/**/*.coffee',
+     './chendian/static/css/src/*.css'],
+    ['replace']
+  );
+});
+
+function escapeRegExp(string){
+  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
+function replaceStaticFiles(src, regexp, dst) {
+  return glob(src, function(er, files) {
     files.map(function(srcName) {
       var name = srcName.split('/');
       name = name[name.length - 1];
-      var js_name = name.split('-')[0];
-      var regex = new RegExp('(js\/frontend\/(?:normal\/)?)' + escapeRegExp(js_name) + '[^\/"\']*\.js', 'ig');
+      var jsName = name.split('-')[0];
+      var regex = regexp;
 
       // 替换模板文件
-      glob('./chendian/templates/frontend/**/*.html', function(er, files) {
+      glob(dst, function(er, files) {
         files.map(function(htmlpath) {
           var source = fs.readFileSync(htmlpath, encoding='utf-8');
           var dest = htmlpath;
+          var regex = regexp(jsName);
           // 替换 js 文件路径
           var data = source.replace(regex, '$1' + name);
           fs.writeFileSync(dest, data);
@@ -49,15 +91,4 @@ gulp.task('replace', ['coffee', 'react'], function() {
 
     });
   });
-});
-
-gulp.task('watch', function() {
-  return gulp.watch(
-    ['./chendian/static/js/**/*.jsx', './chendian/static/js/**/*.coffee'],
-    ['replace']
-  );
-});
-
-function escapeRegExp(string){
-  return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-}
+};
