ce5f11a4ea8d1f252588fa7353a2dbdee0291c9a
mozillazg <opensource.mozillazg@gmail.com>
2015-11-19 16:13:26 +0800

    重构部分 API

--- chendian/api/qq/record.py
+++ chendian/api/qq/record.py
@@ -2,12 +2,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
 
-from django.http import Http404
+from rest_framework import filters
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 
-from rest_framework import status, filters
-from rest_framework.response import Response
-
-from api._base import BaseListAPIView as ListAPIView, BaseAPIView as APIView
+from api._base import BaseListAPIView as ListAPIView
 from qq.models import CheckinRecord
 from qq.utils import record_filter_kwargs
 from .serializers import CheckinSerializer
@@ -26,29 +24,6 @@ class CheckinList(ListAPIView):
         return records
 
 
-class CheckinDetail(APIView):
-
-    def get_object(self, pk):
-        try:
-            return CheckinRecord.objects.get(pk=pk)
-        except CheckinRecord.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        record = self.get_object(pk)
-        serializer = CheckinSerializer(record, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        record = self.get_object(pk)
-        serializer = CheckinSerializer(record, data=request.data,
-                                       context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        record = self.get_object(pk)
-        record.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class CheckinDetail(RetrieveUpdateDestroyAPIView):
+    queryset = CheckinRecord.objects.all()
+    serializer_class = CheckinSerializer
